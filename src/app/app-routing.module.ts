c8866576f1c668d4bc86f32e50c7ebb4c8e00faf
sampath surineni <sampath.skt@gmail.com>
2019-01-02 18:18:36 +0530

    fixed the issue with router not fidning projects URL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { ProjectDetailComponent } from './project-detail/project-detail.componen
 
 
 const routes: Routes = [
-    { path: '', redirectTo: '/projects', pathMatch: 'full' },
+    { path: '', redirectTo: 'projects/', pathMatch: 'full' },
     { path: 'detail/:id', component: ProjectDetailComponent },
     // see if both can be combined into one
     { path: 'projects', redirectTo: 'projects/', pathMatch: 'full' },
